a1b2260f7dddfa5b160b70628a4f33d95877f6a4
Lavanay Thakral <71609599+lavanaythakral@users.noreply.github.com>
2020-11-11 13:57:55 +0530

    Update text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -2,8 +2,9 @@ import pyttsx3
 
 def t2s(text):
 	engine = pyttsx3.init()
-	engine.save_to_file(text,'t2s.mp3')
-	print("File saved")
+	engine.setProperty('rate', 100)
+	engine.setProperty('voice','hindi')
+	engine.save_to_file(text,'Wav2Lip/data/t2s.mp3')
+	# print("File saved")
 	engine.runAndWait()	
 
-	
